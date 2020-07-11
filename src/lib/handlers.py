3eb54ef6799f52f9585fc1479c6b6bca2021d2af
JuiMin <d95wang@gmail.com>
2020-07-11 02:39:53 -0700

    if

--- src/lib/handlers.py
+++ src/lib/handlers.py
@@ -10,14 +10,27 @@ class MainHandler(tornado.web.RequestHandler):
     handler for the homepage
     """
     def get(self):
+        """
+        Serves the homepage
+        """
         data = {"api_base": API_BASE}
         self.render("../templates/home.html", **data)
 
     def post(self):
+        """
+        Endpoint for creating rooms
+        """
         self.set_header("Content-Type", "application/json")
 
         # Create Room
         success = True
+
+        """
+        TODO: 
+        - Implement room creation. Creates two relations
+            - Room Key -> Room Data
+            - Session Key -> Room Key
+        """
         # Select rsp
         if success == True:
             self.set_status(HTTPStatus.CREATED)
@@ -35,24 +48,30 @@ class RoomHandler(tornado.web.RequestHandler):
     def get(self, room_id):
         """
         This should get the room state by ID
+
+        TODO:
+        - Get current game state and connect to websocket
         """
         data = {"api_base": API_BASE, "room": room_id}
         self.render("../templates/room.html", **data)
 
-    def post(self):
-        """
-        Create a room
-        """
-        self.write("post to room")
-
     def patch(self):
         """
         Update a room's information 
+
+        TODO: 
+        - Make this endpoint handle updating the room
+        - Results should be written out to the websockets/channels
+        - UI will update based on the new room state
         """
         self.write("Patch a room")
 
     def delete(self):
         """
         Delete a room
+
+        TODO:
+        - Deletes the Room based on the Room key
+        - This should happen when the last person leaves the room
         """
         self.write("Delete a room")
