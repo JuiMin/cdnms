39f56a7d65b21f987f701c0bc4d2640a2b22b40d
JuiMin <d95wang@gmail.com>
2020-07-10 22:30:08 -0700

    Format stuff?

--- server/main.py
+++ server/main.py
@@ -10,13 +10,14 @@ from lib.handlers import MainHandler, RoomHandler
 
 
 def make_app():
-    return tornado.web.Application([(r"/", MainHandler), (r"/room/([0-9]+)", RoomHandler)])
+    routes = [(r"/", MainHandler), (r"/room/([0-9]+)", RoomHandler)]
+    return tornado.web.Application(routes)
 
 
 if __name__ == "__main__":
     # Get environment variables
     port = os.getenv("PORT", 443)
-    print(f"Starting app on port: {port}")
     app = make_app()
+    print(f"Starting app on port: {port}")
     app.listen(port)
     tornado.ioloop.IOLoop.current().start()
