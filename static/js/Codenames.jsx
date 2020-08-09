d87b723131179016eda9220a57c1257ffcd6ca06
alex.gilbert <agilbe@uw.edu>
2020-08-09 10:54:09 -0700

    add styled-components

--- static/js/Codenames.jsx
+++ static/js/Codenames.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
 export default class Codenames extends React.Component {
     render() {
-        return <div>Why cant I, hold all these codenames?</div>;
+        return <Container>Why cant I, hold all these codenames?</Container>;
     }
 }
+const Container = styled.div(() => {
+    return {
+        border: '1px solid blue'
+    }
+})
