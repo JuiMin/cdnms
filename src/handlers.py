6cc46eb164c4f47dbff3533c66db26cb2729cbff
JuiMin <d95wang@gmail.com>
2020-08-08 17:03:15 -0700

    stub out handler functions and fix models

--- src/handlers.py
+++ src/handlers.py
@@ -10,3 +10,25 @@ class RootHandler(tornado.web.RequestHandler):
         self.set_status(HTTPStatus.OK)
         self.write(f"Welcome to the cdnms API. {str(words.WORDBANK)}")
 
+
+class RoomHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_status(HTTPStatus.OK)
+        self.write(f"Websocket attached")
+
+    def post(self):
+        # TODO: write code for creating a room
+        pass
+
+
+class PlayerHandler(tornado.web.RequestHandler):
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class GameHandler(tornado.web.RequestHandler):
+    def post(self):
+        pass
--- src/models.py
+++ src/models.py
@@ -2,27 +2,39 @@
 Codenames model definitions
 """
 
+from enum import Enum
 import random
-from typing import List
+from typing import Dict, List
 
 import words
 
 
-class Player:
-    __slots__ = "name"
+class Card:
+    __slots__ = ("flipped", "value", "association")
 
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self, value, association):
+        self.value = value
+        self.association = association
+        self.flipped = False
+
+    def flip(self):
+        self.flipped = True
 
 
 class Codenames:
-    __slots__ = "words"
+    __slots__ = ("cards", "gameover")
 
     def __init__(self):
         self.setup()
 
     def setup(self):
-        self.words = random.choices(words.WORDBANK, k=25)
+        self.gameover = False
+        self.cards = random.choices(words.WORDBANK, k=25)
+
+
+class Team(Enum):
+    RED = 1
+    BLUE = 0
 
 
 class Room:
@@ -30,15 +42,12 @@ class Room:
         "capacity",
         "game_instance",
         "name",
-        "red_team",
-        "blue_team",
-        "spectators",
+        "players",
     )
 
     def __init__(self, name: str, capacity: int = 10):
         self.capacity = capacity
         self.name = name
         self.game_instance = Codenames()
-        self.red_team = []
-        self.blue_team = []
-        self.spectators = []
+        # TEAMS
+        self.players: Dict[str, Team] = {}
