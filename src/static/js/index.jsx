12e19244f9101714a944a61be78555127cb861a7
alex.gilbert <agilbe@uw.edu>
2020-08-09 22:58:00 -0700

    down with js

--- src/static/js/index.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Codenames from "./Codenames";
-ReactDOM.render(<Codenames baseURL={window.baseURL} />, document.getElementById("content"));
--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -3,27 +3,31 @@ const getConfig = (env) => {
     console.log(env)
     return {
         mode: env && env.NODE_ENV == 'development' ? 'development' : 'production',
-        entry: __dirname + '/js/index.jsx',
+        entry: __dirname + '/src/index.tsx',
         output: {
             path: __dirname + '/dist',
             filename: 'bundle.js',
         },
         resolve: {
-            extensions: ['.js', '.jsx', '.css']
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.css']
         },
         module: {
             rules: [
                 {
-                    test: /\.jsx?/,
+                    test: /\.tsx?/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
                         options: {
                             "presets": [
                                 "@babel/preset-env",
-                                "@babel/preset-react"
+                                "@babel/preset-typescript",
+                                "@babel/preset-react",
                             ],
-                            "plugins": ["@babel/plugin-proposal-class-properties"]
+                            "plugins": [
+                                "@babel/proposal-class-properties",
+                                "@babel/proposal-object-rest-spread"
+                            ]
                         }
                     }
                 }
