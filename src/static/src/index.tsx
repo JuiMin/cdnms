0cc423940decd614e9c5afbe6dbc04af47cf3da4
alex.gilbert <agilbe@uw.edu>
2020-08-09 22:56:19 -0700

    types for dinner

---
+++ src/static/src/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Codenames from "./Codenames";
+ReactDOM.render(<Codenames baseURL={window.baseURL} />, document.getElementById("content"));
