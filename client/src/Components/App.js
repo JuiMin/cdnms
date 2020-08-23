f591629d7ca1b90309f8b1775bd3b71c4fc4f5b4
JuiMin <d95wang@gmail.com>
2020-08-22 22:18:10 -0700

    Add material UI icons

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import { Button } from '@material-ui/core';
+
 function App() {
   return (
     <div className="App">
-
+      <Button color="primary">Hello World</Button>
     </div>
   );
 }
